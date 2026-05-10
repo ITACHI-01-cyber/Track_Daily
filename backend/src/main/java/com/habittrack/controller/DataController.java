2f7237525a958996fb5e0e2973407635f157b437
ITACHI-01-cyber <bvivek514@gmail.com>
2026-05-10 14:34:13 +0530

    Added DataController to fix MongoDB sync and secured Gemini AI endpoint

--- backend/src/main/java/com/habittrack/controller/DataController.java
+++ backend/src/main/java/com/habittrack/controller/DataController.java
@@ -2,12 +2,8 @@ package com.habittrack.controller;
 
 import java.util.Map;
 
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,23 +18,20 @@ public class DataController {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    @GetMapping(produces = "application/json")
-    public ResponseEntity<String> getData() {
-        Query query = new Query(Criteria.where("id").is(1));
-        Document doc = mongoTemplate.findOne(query, Document.class, "app_data");
-        if (doc != null && doc.containsKey("content")) {
-            return ResponseEntity.ok(doc.getString("content"));
-        }
-        return ResponseEntity.ok("{}");
+    @PostMapping
+    public ResponseEntity<?> saveData(@RequestBody Map<String, Object> data) {
+        // Save all incoming frontend data into a single master document
+        data.put("_id", "user_dashboard_data");
+        mongoTemplate.save(data, "app_data");
+        return ResponseEntity.ok().build();
     }
 
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Map<String, Boolean>> saveData(@RequestBody String rawJson) {
-        Query query = new Query(Criteria.where("id").is(1));
-        Update update = new Update().set("id", 1).set("content", rawJson);
-        
-        mongoTemplate.upsert(query, update, "app_data");
-        
-        return ResponseEntity.ok(Map.of("success", true));
+    @GetMapping
+    public ResponseEntity<?> getData() {
+        Map<String, Object> data = mongoTemplate.findById("user_dashboard_data", Map.class, "app_data");
+        if (data == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(data);
     }
 }
